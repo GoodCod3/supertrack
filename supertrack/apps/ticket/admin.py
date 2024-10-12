582a384c64bb9efa7328e596ece0e04fa1872b63
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-10-12 21:58:42 +0200

    Adding Ticket model migration files

--- supertrack/apps/ticket/admin.py
+++ supertrack/apps/ticket/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 
-from supertrack.apps.ticket.models import TicketModel
+from supertrack.apps.ticket.models import TicketModel, TicketProductModel
 from supertrack.helpers.admin import BaseModelAdmin
 
 
+class TicketProductModelInline(admin.TabularInline):
+    model = TicketProductModel
+    extra = 1
+    
+class TicketProductModelAdmin(BaseModelAdmin):
+    model = TicketProductModel
+    empty_value_display = "-empty-"
+    
+    list_display = (
+        "public_id",
+        "name",
+        "unit_price",
+        "total_price",
+    )
+    search_fields = ("name",)
+    # list_filter = ()
+
+
+admin.site.register(TicketProductModel, TicketProductModelAdmin)
+
 class TicketModelAdmin(BaseModelAdmin):
     model = TicketModel
+    inlines = [
+        TicketProductModelInline,
+    ]
     empty_value_display = "-empty-"
     list_display = (
         "public_id",
--- supertrack/apps/ticket/models.py
+++ supertrack/apps/ticket/models.py
@@ -8,7 +8,15 @@ from supertrack.helpers.filer import ticket_image_upload_path
 class TicketModel(BaseModel):
     image = models.ImageField(
         upload_to=ticket_image_upload_path,
-        blank=True,
     )
     total = models.FloatField(_("Total"), default=0.0)
     paid_at = models.DateTimeField(blank=True, null=True)
+
+class TicketProductModel(BaseModel):
+    ticket = models.ForeignKey(
+        TicketModel,
+        on_delete=models.PROTECT,
+    )
+    name = models.CharField(_("Name"), max_length=200)
+    unit_price = models.FloatField(_("Price"), default=0.0)
+    total_price = models.FloatField(_("Price"), default=0.0)
