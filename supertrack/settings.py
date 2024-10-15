069a44b2fb08113906a784eff33f5e84a05f4661
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-10-15 14:07:53 +0200

    Configuring and serving statics

--- supertrack/settings.py
+++ supertrack/settings.py
@@ -131,7 +131,9 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles/dashboard"
+]
 MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
