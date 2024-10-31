730740ea3709a97c2eba9722b9fc7f2f4b1c217b
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-10-31 14:04:46 +0100

    Adding cache for supermarket products

--- supertrack/apps/shopping_list/views/get_consum_products.py
+++ supertrack/apps/shopping_list/views/get_consum_products.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 from django.contrib.auth.decorators import login_required
@@ -12,28 +13,33 @@ from supertrack.apps.scrappy.models import ConsumProductModel
 @login_required
 def get_consum_products(request):
     if request.user.is_authenticated:
-        products = {}
-        consum_products = ConsumProductModel.objects.all()[:100]
+        products = cache.get('consum_all_products')
 
-        for consum_product in consum_products:
-            parent_category = consum_product.category.parent_category.parent_category.name
-            product_category = consum_product.category.parent_category.name
-            
-            if parent_category not in products:
-                products[parent_category] = {}
-            
-            if product_category not in products[parent_category]:
-                products[parent_category][product_category] = []
+        if products is None:
+            products = {}
+            consum_products = ConsumProductModel.objects.all()
+            for consum_product in consum_products:
+                parent_category = consum_product.category.parent_category.parent_category.name
+                product_category = consum_product.category.parent_category.name
+                
+                
+                if parent_category not in products:
+                    products[parent_category] = {}
+                
+                if product_category not in products[parent_category]:
+                    products[parent_category][product_category] = []
 
-            products[parent_category][product_category].append(
-                {
-                    "id": consum_product.public_id,
-                    "name": consum_product.name,
-                    "image": consum_product.image,
-                    "price": consum_product.unit_price,
-                }
-            )
-    
+                products[parent_category][product_category].append(
+                    {
+                        "id": consum_product.public_id,
+                        "name": consum_product.name,
+                        "image": consum_product.image,
+                        "price": consum_product.unit_price,
+                    }
+                )
+
+            cache.set("consum_all_products", products, timeout=3600)
+
         return JsonResponse(products)
 
     raise Exception("You are not authenticated.")
